d054a3270a1fea4066c29447a00b102c70944a10
Mats100 <muneeb75@icloud.com>
2022-07-22 20:07:09 +0500

    first commit

---
+++ list.py
@@ -0,0 +1,3 @@
+office=["Omer","Hasnain","Imran","Bilal","Shahid","Asim"]
+office.append(["Mahad","Muzammil"])
+print(office)
---
+++ main.py
@@ -0,0 +1,16 @@
+print("Hello World")
+b="Mats"
+s="muneeb"
+
+
+print(len(b))
+print(len(s))
+num = 98765
+print(str(num)[::-1])
+x=5
+y=6
+temp=x
+x=y
+y=temp
+print(f"The value of x After Swap:{x}" )
+print(f"The value of y After Swap:{y}")
